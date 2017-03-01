adeb32aaf9008289000f91e0cc4beaca7327579b
Marcos <maigtello@hotmail.com>
2017-03-01 11:23:49 +0100

    barra lateral

--- app/src/main/java/com/example/marcos/simulador/MainActivity.java
+++ app/src/main/java/com/example/marcos/simulador/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.marcos.simulador;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -20,12 +23,26 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonCalcular;
     private TextView textViewR;
     private String pantalla = "";
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
 
         //jugador1
         spinner0 = (Spinner) findViewById(R.id.spinner0);
